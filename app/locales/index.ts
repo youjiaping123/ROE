b1ca9c95634d92a49c251eafde53c7917e83bf8f
roe fruit <youjiaping9084@outlook.com>
2024-09-26 10:29:24 +0000

    语言

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,21 +1,8 @@
 import cn from "./cn";
 import en from "./en";
-import pt from "./pt";
 import tw from "./tw";
-import id from "./id";
-import fr from "./fr";
-import es from "./es";
-import it from "./it";
 import tr from "./tr";
-import jp from "./jp";
-import de from "./de";
 import vi from "./vi";
-import ru from "./ru";
-import no from "./no";
-import cs from "./cs";
-import ko from "./ko";
-import ar from "./ar";
-import bn from "./bn";
 import sk from "./sk";
 import { merge } from "../utils/merge";
 import { safeLocalStorage } from "@/app/utils";
@@ -29,21 +16,8 @@ const ALL_LANGS = {
   cn,
   en,
   tw,
-  pt,
-  jp,
-  ko,
-  id,
-  fr,
-  es,
-  it,
   tr,
-  de,
   vi,
-  ru,
-  cs,
-  no,
-  ar,
-  bn,
   sk,
 };
 
@@ -54,22 +28,9 @@ export const AllLangs = Object.keys(ALL_LANGS) as Lang[];
 export const ALL_LANG_OPTIONS: Record<Lang, string> = {
   cn: "简体中文",
   en: "English",
-  pt: "Português",
   tw: "繁體中文",
-  jp: "日本語",
-  ko: "한국어",
-  id: "Indonesia",
-  fr: "Français",
-  es: "Español",
-  it: "Italiano",
   tr: "Türkçe",
-  de: "Deutsch",
   vi: "Tiếng Việt",
-  ru: "Русский",
-  cs: "Čeština",
-  no: "Nynorsk",
-  ar: "العربية",
-  bn: "বাংলা",
   sk: "Slovensky",
 };
 
@@ -139,22 +100,9 @@ const DEFAULT_STT_LANG = "zh-CN";
 export const STT_LANG_MAP: Record<Lang, string> = {
   cn: "zh-CN",
   en: "en-US",
-  pt: "pt-BR",
   tw: "zh-TW",
-  jp: "ja-JP",
-  ko: "ko-KR",
-  id: "id-ID",
-  fr: "fr-FR",
-  es: "es-ES",
-  it: "it-IT",
   tr: "tr-TR",
-  de: "de-DE",
   vi: "vi-VN",
-  ru: "ru-RU",
-  cs: "cs-CZ",
-  no: "no-NO",
-  ar: "ar-SA",
-  bn: "bn-BD",
   sk: "sk-SK",
 };
 
